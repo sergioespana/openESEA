c623f1860c57fb2e4b98f4b9c40aabd328a7e903
ArturUser <arturmoeijes@outlook.com>
2021-12-11 17:29:48 +0100

    Update audit_iso_forest.py
    Edited isolation forest for openESEA

--- backend/core/utils/audit_iso_forest.py
+++ backend/core/utils/audit_iso_forest.py
@@ -1,3 +1,24 @@
+from sklearn.ensemble import IsolationForest
+import pandas as pd
 
 def calculate_iso_forest():
+    #Creating the model
+    model=IsolationForest(n_estimators=50, 
+                        max_samples='auto', 
+                        contamination=float(0.1),
+                        max_features=1.0)
+
+    #fitting the model on one feature for unsupervised learning
+    model.fit(df[['column']])
+
+
+    df['scores']=model.decision_function(df[['column']])
+    df['anomaly']=model.predict(df[['column']])
+    df2.tail()
+
+    anomaly=df.loc[df['anomaly']==-1]
+
+    anomaly_index=list(anomaly.index)
+
+    print(anomaly)
     return True

950c0bf5c0cd235ba39a90937e838d7587c04e18
Marc de Fluiter <marcdefluiter@gmail.com>
2023-06-07 16:58:50 +0200

    a

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,14 @@
+const path = require('path');
+
 module.exports = {
     devServer: {
       proxy: process.env.VUE_APP_ROOT_API // 'https://esea-api.herokuapp.com/' // 'http://localhost:8000' // http://127.0.0.1:8000/
     },
-    chainWebpack: config => {
-        config.module
-            .rule('vue')
-            .use('vue-loader')
-            .tap(options => {
-                options.compiler = require('vue-template-babel-compiler')
-                return options
-            })
+    configureWebpack: {
+      resolve: {
+        alias: {
+          'yaml': path.resolve(__dirname, 'node_modules/js-yaml/dist/js-yaml.js')
+        }
+      }
     }
 }

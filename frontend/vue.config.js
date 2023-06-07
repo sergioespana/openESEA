0467e9babe4ccf1d774bafe9c93c779e35f6dd3a
Marc de Fluiter <marcdefluiter@gmail.com>
2023-06-07 13:31:03 +0200

    hmm

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     devServer: {
       proxy: process.env.VUE_APP_ROOT_API // 'https://esea-api.herokuapp.com/' // 'http://localhost:8000' // http://127.0.0.1:8000/
+    },
+    chainWebpack: config => {
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .tap(options => {
+                options.compiler = require('vue-template-babel-compiler')
+                return options
+            })
     }
 }
